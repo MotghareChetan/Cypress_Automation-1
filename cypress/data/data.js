c9984d656693de89b92638a9fcd7c5ff8270dca3
MotghareChetan <chetan@appmetry.com>
2023-08-24 10:49:54 +0530

    Cypress basic element interaction

---
+++ cypress/data/data.js
@@ -0,0 +1,17 @@
+export const userData = {
+    username: "admin",
+    password: "password123"
+  };
+  
+  export const bookingUpdateData = {
+    firstname: "James",
+    lastname: "Brown",
+    totalprice: 111,
+    depositpaid: true,
+    bookingdates: {
+      checkin: "2018-01-01",
+      checkout: "2019-01-01"
+    },
+    additionalneeds: "Breakfast"
+  };
+  
