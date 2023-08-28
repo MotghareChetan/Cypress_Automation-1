f1712eaa8f5eb31c4f1105a438c1775e5a3dcca4
MotghareChetan <82577168+ChetanMotghare@users.noreply.github.com>
2023-08-28 12:30:33 +0530

    Update 09_fileUpload.cy.js

--- cypress/e2e/practice_test/09_fileUpload.cy.js
+++ cypress/e2e/practice_test/09_fileUpload.cy.js
@@ -3,13 +3,13 @@ describe("FileUpload Download",()=>{
 
     it("FileUpload",()=>{
         cy.visit("https://the-internet.herokuapp.com/upload");
-        cy.get("#file-upload").selectFile("C:/Users/Appmetry/Project/Cypress_Automation/package.json")
-        cy.get("#file-submit").click();
+        /*cy.get("#file-upload").selectFile("C:/Users/Appmetry/Project/Cypress_Automation/package.json")
+        cy.get("#file-submit").click();*/
     })
 
     it.only("Droping File",()=>{
         cy.visit("https://the-internet.herokuapp.com/upload");
         //cy.get("#file-upload").selectFile("C:/Users/Appmetry/Project/Cypress_Automation/package.json")
-        cy.document().selectFile('C:/Users/Appmetry/Project/Cypress_Automation/package.json', { action: 'drag-drop' })
+      //  cy.document().selectFile('C:/Users/Appmetry/Project/Cypress_Automation/package.json', { action: 'drag-drop' })
     })
 })
